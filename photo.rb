688c2e98dbb1132cf3157d32478577fb7891ed1b
Chris Mear <chris@feedmechocolate.com>
2011-03-26 23:49:15 +0000

    Don't store photos that have missing data.

--- photo.rb
+++ photo.rb
@@ -1,13 +1,16 @@
 require 'dm-core'
+require 'dm-validations'
 
 class Photo
   include DataMapper::Resource
   
   property :id, Serial
-  property :instagram_id, Integer
-  property :image_url, String, :length => 255
-  property :link_url, String, :length => 255
-  property :username, String, :length => 255
+  property :instagram_id, Integer, :required => true
+  property :image_url, String, :length => 255, :required => true
+  property :link_url, String, :length => 255, :required => true
+  property :username, String, :length => 255, :required => true
   
   belongs_to :tag
+  
+  validates_uniqueness_of :instagram_id
 end
