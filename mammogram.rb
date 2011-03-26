e6690a7b055066f54581c439d54f6387f222ad3f
Chris Mear <chris@feedmechocolate.com>
2011-03-26 23:48:54 +0000

    Show a few photos, randomly. Badly.

--- mammogram.rb
+++ mammogram.rb
@@ -7,7 +7,8 @@ set :haml, :format => :html5
 
 class Mammogram < Sinatra::Base
   get '/' do
-    @photos = Photo.all
+    # FIXME Make random not hilarious bad here.
+    @photos = Photo.all.to_a.sort_by{rand}[0..15]
     haml :photos
   end
 end
