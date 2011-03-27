cdb8576a4fa73de225dc3441d869042510f5b591
Chris Mear <chris@feedmechocolate.com>
2011-03-27 00:22:34 +0000

    Fix serving of static assets.

--- mammogram.rb
+++ mammogram.rb
@@ -3,9 +3,11 @@ require 'haml'
 
 require 'init'
 
-set :haml, :format => :html5
-
 class Mammogram < Sinatra::Base
+  set :haml, :format => :html5
+  set :root, File.dirname(__FILE__)
+  # set :static, true
+  
   get '/' do
     # FIXME Make random not hilarious bad here.
     @photos = Photo.all.to_a.sort_by{rand}[0..15]
